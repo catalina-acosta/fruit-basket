30ec54913292969c4b1687f01d90ac1f2847aa74
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-12 13:33:04 +0100

    added starting kit files

---
+++ src/app/fruit-list/fruit-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fruit-list',
+  imports: [],
+  templateUrl: './fruit-list.component.html',
+  styleUrl: './fruit-list.component.scss'
+})
+export class FruitListComponent {
+
+}
